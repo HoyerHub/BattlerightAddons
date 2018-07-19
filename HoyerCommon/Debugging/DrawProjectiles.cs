47d936f48cea861841c60596e1274c0574a5e3a8
Nick Hoyer <twicci@me.com>
2018-07-20 01:20:41 +0200

    fix compile error caused by gitignore

---
+++ HoyerCommon/Debugging/DrawProjectiles.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using BattleRight.Core;
+using Hoyer.Common.Data.Abilites;
+using Hoyer.Common.Extensions;
+using UnityEngine;
+using Vector2 = BattleRight.Core.Math.Vector2;
+
+namespace Hoyer.Common.Debug
+{
+    public static class DrawProjectiles
+    {
+        static DrawProjectiles()
+        {
+            Game.OnDraw += Game_OnDraw;
+        }
+
+        private static void DrawRectangle(Vector2 start, Vector2 end, float radius, Color color)
+        {
+            var w = end.X - start.X;
+            var h = end.Y - start.Y;
+            var l = Math.Sqrt(w * w + h * h);
+            var xS = (radius * h / l);
+            var yS = (radius * w / l);
+
+            var rightStartPos = new Vector2((float) (start.X - xS), (float) (start.Y + yS));
+            var leftStartPos = new Vector2((float)(start.X + xS), (float)(start.Y - yS));
+            var rightEndPos = new Vector2((float)(end.X - xS), (float)(end.Y + yS));
+            var leftEndPos = new Vector2((float)(end.X + xS), (float)(end.Y - yS));
+
+            Drawing.DrawLine(rightStartPos, rightEndPos, color);
+            Drawing.DrawLine(leftStartPos, leftEndPos, color);
+            Drawing.DrawLine(rightStartPos, leftStartPos, color);
+            Drawing.DrawLine(leftEndPos, rightEndPos, color);
+        }
+
+        public static UnityEngine.Vector2 ConvertToUnity2D(this Vector2 source)
+        {
+            return new UnityEngine.Vector2(source.X, Screen.height - source.Y);
+        }
+
+        private static void Game_OnDraw(EventArgs args)
+        {
+            if(!Game.IsInGame)return;
+            foreach (var projectile in EntitiesManager.ActiveProjectiles)
+            {
+                if (projectile.BaseObject.TeamId == 2)
+                {
+                    DrawRectangle(projectile.StartPosition, projectile.CalculatedEndPosition, projectile.Radius, Color.red);
+                    Drawing.DrawCircle(projectile.MapObject.Position, projectile.Radius / 2, Color.red);
+                }
+                else if (projectile.BaseObject.TeamId == 1)
+                {
+                    DrawRectangle(projectile.StartPosition, projectile.CalculatedEndPosition, projectile.Radius, Color.green);
+                    Drawing.DrawCircle(projectile.MapObject.Position, projectile.Radius / 2, Color.green);
+                }
+            }
+
+            foreach (var character in EntitiesManager.EnemyTeam)
+            {
+                if (character.CharacterModel.IsModelInvisible)
+                {
+                    Drawing.DrawCircle(character.Pos(), character.MapCollision.MapCollisionRadius, Color.green);
+                    Drawing.DrawString(character.Pos(), character.CharName, Color.green);
+                }
+            }
+                
+            foreach (var cast in AbilityTracker.Enemy.Projectiles.Casting)
+            {
+                DrawRectangle(cast.Caster.Pos(), cast.EndPos, cast.Data.Radius, Color.blue);
+            }
+            /*var guiStyle = GUI.skin.GetStyle("label");
+            guiStyle.fontSize = 15;
+            guiStyle.normal.textColor = Color.green;
+
+            var i = 0;
+            GUI.Label(new Rect(new UnityEngine.Vector2(Screen.width - 200, 50), new UnityEngine.Vector2(100, 50)), EntitiesManager.LocalPlayer.CharName, guiStyle);
+            foreach (var buff in EntitiesManager.LocalPlayer.Buffs)
+            {
+                i++;
+                GUI.Label(new Rect(new UnityEngine.Vector2(Screen.width - 200, i*70), new UnityEngine.Vector2(100, 50)), buff.ObjectName, guiStyle);
+                i++;
+                GUI.Label(new Rect(new UnityEngine.Vector2(Screen.width - 200, i * 70), new UnityEngine.Vector2(100, 50)), buff.BuffType.ToString(), guiStyle);
+            }*/
+        }
+    }
+
+    
+}
---
+++ HoyerCommon/Debugging/LogProjectiles.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BattleRight.Core;
+using BattleRight.SDK;
+using Hoyer.Common.Data.Abilites;
+using Projectile = BattleRight.Core.GameObjects.Projectile;
+
+namespace Hoyer.Common.Debug
+{
+    public static class LogProjectiles
+    {
+        public static void Init()
+        {
+            //InGameObject.OnCreate += InGameObject_OnCreate;
+            //Game.OnUpdate += Game_OnUpdate;
+        }
+
+        private static void Game_OnUpdate(EventArgs args)
+        {
+            foreach (var activeProjectile in EntitiesManager.ActiveProjectiles)
+            {
+                if (activeProjectile.MapObject.Position.Distance(activeProjectile.CalculatedEndPosition) < 0.2)
+                {
+                    if (Math.Abs(activeProjectile.Age.Age) > 0.001)
+                    {
+                        Console.WriteLine(activeProjectile.ObjectName);
+                        Console.WriteLine(activeProjectile.Range);
+                        Console.WriteLine(activeProjectile.Radius);
+                        Console.WriteLine(activeProjectile.SpellCollision.SpellCollisionRadius);
+                        Console.WriteLine(activeProjectile.MapObject.Position.Distance(activeProjectile.StartPosition) / activeProjectile.Age.Age);
+                        Console.WriteLine("----");
+                    }
+                }
+            }
+        }
+
+        public static readonly List<Projectile> ActiveProjectiles = new List<Projectile>();
+
+        public static void CheckProjectiles()
+        {
+            foreach (var activeGameObject in EntitiesManager.ActiveProjectiles)
+            {
+                if (ActiveProjectiles.Any(p => p.ObjectName == activeGameObject.ObjectName))
+                {
+                    return;
+                }
+                var info = AbilityDatabase.Get(activeGameObject.ObjectName);
+                if (info != null)
+                {
+                    Console.WriteLine("Found " + info.ObjectName + " as " + info.Champion + "'s " + info.AbilitySlot + " in the Database!");
+                    ActiveProjectiles.Add(activeGameObject);
+                }
+                else Console.WriteLine("Didn't find " + activeGameObject.ObjectName + " in the Database!");
+            }
+        }
+    }
+}
