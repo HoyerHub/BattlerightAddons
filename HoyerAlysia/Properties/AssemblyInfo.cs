5a547d3fa37b2042e319a45d2cbdb15a7b927e83
Nick Hoyer <twicci@me.com>
2018-08-07 20:32:40 +0200

    Small updates

--- HoyerAlysia/Properties/AssemblyInfo.cs
+++ HoyerAlysia/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HoyerAlysia")]
+[assembly: AssemblyTitle("HoyerAlysia (WIP)")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HoyerAlysia")]
+[assembly: AssemblyProduct("HoyerAlysia (WIP)")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
